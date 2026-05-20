0c41cd476e9e92b52211315844b045574146ec50
Oskar Ortiz <okyjulian994@gmail.com>
2026-05-20 11:34:27 -0500

    fix(deploy): bootstrap database on Render startup

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
+
+def run_startup_tasks():
+    if os.environ.get("RUN_STARTUP_TASKS", "false").lower() != "true":
+        return
+
+    # Render free plans do not execute pre-deploy commands, so we bootstrap the
+    # schema and demo catalog during process startup.
+    call_command("migrate", interactive=False, verbosity=0)
+    call_command("seed_initial_data", verbosity=0)
+
+
+run_startup_tasks()
 application = get_wsgi_application()
