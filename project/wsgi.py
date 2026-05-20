b8e3ecc34781d7cf2099cfebe482e6566fa84cca
Oskar Ortiz <okyjulian994@gmail.com>
2026-05-20 11:39:09 -0500

    fix(deploy): initialize django before startup bootstrap

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+import django
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
@@ -21,6 +22,7 @@ def run_startup_tasks():
 
     # Render free plans do not execute pre-deploy commands, so we bootstrap the
     # schema and demo catalog during process startup.
+    django.setup()
     call_command("migrate", interactive=False, verbosity=0)
     call_command("seed_initial_data", verbosity=0)
 
